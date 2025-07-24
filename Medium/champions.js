3ca64385c25e318f88a6fa25f66d6e897a0d54d0
Gabriela <gabriela@gmail.com>
2025-07-24 15:09:58 +0300

    Refactor champions function to improve readability and handle empty input

--- Medium/champions.js
+++ Medium/champions.js
@@ -1,5 +1,7 @@
 /*
-Create a function that takes an array of football clubs with properties: name, wins, loss, draws, scored, conceded, and returns the team name with the highest number of points. If two teams have the same number of points, return the team with the largest goal difference.
+Create a function that takes an array of football clubs with properties: name, wins, loss, draws, scored, conceded, 
+and returns the team name with the highest number of points. 
+If two teams have the same number of points, return the team with the largest goal difference.
 How to Calculate Points and Goal Difference
 team = {name: "Manchester United", wins: 30, loss: 3, draws: 5, scored: 88, conceded: 20 }
 
@@ -35,8 +37,41 @@ champions([
 ➞ "Manchester United"
 */
 
-function champions( /*args*/ ) {
-  //your code
+function champions(footballTeams) {
+  if (!footballTeams || footballTeams.length === 0)  return null; 
+
+  let maxScore = -1;
+  let maxScoreTeams = [];
+  for(const team of footballTeams){
+    let teamScore =  team.wins*3 + team.draws;
+    if (maxScore < teamScore){
+      maxScore = teamScore;
+      maxScoreTeams = [] ;
+      maxScoreTeams.push(team);     
+    }
+    else if (maxScore == teamScore){
+      maxScoreTeams.push(team);  
+    }      
+  }
+
+  if (maxScoreTeams.length === 1)
+    return maxScoreTeams[0].name;
+
+  let highestGoalDiffTeam = maxScoreTeams[0];
+  let highestGoalDiff = highestGoalDiffTeam.scored - highestGoalDiffTeam.conceded;
+
+    for (let i = 1; i < maxScoreTeams.length; i++) {
+      const currentTeam = maxScoreTeams[i];
+      const goalDiff = currentTeam.scored - currentTeam.conceded; 
+      if (goalDiff > highestGoalDiff){
+        highestGoalDiff = goalDiff;
+        highestGoalDiffTeam = currentTeam;
+      }       
+  }
+
+  return highestGoalDiffTeam.name;
+
+
 }
 
 exports.solution = champions;
