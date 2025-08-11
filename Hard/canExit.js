22a21a1ef2b0ae931131ba1915f6b1b2567bb989
Gabriela <gabriela@gmail.com>
2025-08-11 15:50:15 +0300

    done

--- Hard/canExit.js
+++ Hard/canExit.js
@@ -44,8 +44,28 @@ In a maze of size m x n, you enter at [0, 0] and exit at [m-1, n-1].
 There can be dead ends in a maze - one exit path is sufficient.
 */
 
-function canExit( /*args*/ ) {
-  //your code
+function canExit(MazeMatrix) {
+  const rows = MazeMatrix.length;
+  const cols = MazeMatrix[0].length;
+  const exitRow = rows - 1;
+  const exitCol = cols - 1;
+
+  function dfs(r, c) {
+    if (r < 0 || r >= rows || c < 0 || c >= cols || MazeMatrix[r][c] !== 0)  return false;
+   
+    if (r === exitRow && c === exitCol)  return true;
+  
+    MazeMatrix[r][c] = '$';
+
+    return (
+      dfs(r + 1, c) ||
+      dfs(r - 1, c) ||
+      dfs(r, c + 1) ||
+      dfs(r, c - 1)
+    );
+  }
+
+  return dfs(0, 0);
 }
 
 exports.solution = canExit;
