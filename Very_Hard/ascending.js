c1766d79cfbef929af1c01e05faa2d0bb065c554
Gabriela <gabriela@gmail.com>
2025-08-25 16:26:07 +0300

    solved ascending problem

--- Very_Hard/ascending.js
+++ Very_Hard/ascending.js
@@ -14,8 +14,25 @@ Notes
 A number can consist of any number of digits, so long as the numbers are adjacent to each other, and the string has at least two of them.
 */
 
-function ascending( /*args*/ ) {
-  //your code
+function ascending(number) {
+    for (let i = 1; i <= Math.floor(number.length / 2); i++) {
+        const firstNum = parseInt(number.slice(0, i));
+        let expectedNum = firstNum + 1;
+        let currentString = number.slice(i);
+        
+        while (currentString.length > 0) {
+            const numStr = expectedNum.toString();
+            if (currentString.startsWith(numStr)) {
+                currentString = currentString.slice(numStr.length);
+                expectedNum++;
+            } else 
+                break;            
+        }        
+        if (currentString.length === 0) return true;        
+    }    
+    return false;
 }
+
+exports.solution = ascending;
 
 exports.solution = ascending;
